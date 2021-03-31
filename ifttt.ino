8a5dd9dd426736c0edc711c9646a35523b4a288b
dagbay <80764810+dagbay@users.noreply.github.com>
2021-03-31 23:33:08 +1100

    Update ifttt.ino

--- ifttt.ino
+++ ifttt.ino
@@ -1,20 +1,38 @@
 int photo_resistor = A1;
 int photo_resistor_power = A0;
+int led = D7;
 
 void setup() {
     
+    // We provide the resistor and LED pins and set their pins.
     pinMode(photo_resistor, INPUT);
     pinMode(photo_resistor_power, OUTPUT);
+    pinMode(led, OUTPUT);
     
-    digitalWrite(photo_resistor_power, HIGH);
+    // We give the photo resistor power before we start.
+    digitalWrite(photo_resistor_power, HIGH); 
 }
 
 void loop() {
+    
+    // Declare int variable light_level and always assign it with the latest values of photo_resistor.
     int light_level = analogRead(photo_resistor);
+    // Declare int variable i for comparison against light_level.
     int i = 1;
     
-    if (light_level > i) Particle.publish("sun", "up", PRIVATE);
-    if (light_level < i) Particle.publish("sun", "down", PRIVATE);
+    // If light level is greater than i, then publish to IFTTT that the sun is up.
+    if (light_level > i) {
+        Particle.publish("sun", "up", PRIVATE);
+        // Let the user know by turning on the LED Light.
+        digitalWrite(led, HIGH);
+    }
     
-    delay(10000);
+    // Otherwise, if less than i, then publish to IFTTT that the sun is down.
+    else if (light_level < i) {
+        Particle.publish("sun", "down", PRIVATE);
+        digitalWrite(led, LOW);
+    }
+    
+    // Delay for 5 seconds.
+    delay(5000);
 }
